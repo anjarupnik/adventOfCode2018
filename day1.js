1f4cdf3776d522f944c898dc640c76603c9881c9
anjarupe <anja.rupnik@debijenkorf.nl>
2019-01-19 20:15:37 +0100

    day 1 part 1

---
+++ day1.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./day1Input.txt');
+let data = '';
+let result = 0;
+
+const calculate = () => {
+        data.split('\n').forEach((number) => {
+            result += parseInt(number);
+        });
+        return result;
+}
+
+readStream
+    .on('data', (chunk) => {
+        data += chunk;
+})
+    .on('end', () => {
+        console.log(calculate());
+});

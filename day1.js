e7a396308667a5384efe917683f48768b91312b2
anjarupe <anja.rupnik@debijenkorf.nl>
2019-01-21 21:49:04 +0100

    first day second part

--- day1.js
+++ day1.js
@@ -1,20 +1,51 @@
 const fs = require('fs');
 
 const readStream = fs.createReadStream('./day1Input.txt');
+let numbers = [];
 let data = '';
 let result = 0;
+let reachedFrequencies = [];
+let equalFrequency = null;
 
 const calculate = () => {
-    data.split('\n').forEach((number) => {
-        result += parseInt(number);
-    });
+    numbers = data.split('\n');
+    numbers.forEach((number) => { result += parseInt(number) });
+
     return result;
 }
+
+const loopOverNumbers = () => {
+    let i = 0;
+    while (i < numbers.length) {
+        const n = result += parseInt(numbers[i]);
+        const index = reachedFrequencies.indexOf(n);
+        if (index !== -1) {
+            equalFrequency = reachedFrequencies[index];
+            break;
+        }
+        reachedFrequencies.push(n);
+        i++;
+    }
+}
+
+const findFrequency = () => {
+    result = 0;
+
+    while (!equalFrequency) {
+        loopOverNumbers();
+    }
+
+    return equalFrequency;
+};
 
 readStream
     .on('data', (chunk) => {
         data += chunk;
 })
     .on('end', () => {
+        // first part
         console.log(calculate());
+
+        // second part
+        console.log(findFrequency());
 });
